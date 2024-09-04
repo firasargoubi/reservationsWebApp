f6557e749e086325cd0a3d341ab012b24f10f863
Firas Argoubi <99605613+firasargoubi@users.noreply.github.com>
2024-09-04 18:35:17 -0400

    Update settings.py

--- garageCalendar/settings.py
+++ garageCalendar/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'garageCalendar' / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'garageCalendar' / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
