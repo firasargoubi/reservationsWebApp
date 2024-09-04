5050b68fb35f2680e0a5ad6990ece9e9633f5cd8
Firas Argoubi <99605613+firasargoubi@users.noreply.github.com>
2024-09-04 19:57:58 -0400

    Update views.py

--- reservations/views.py
+++ reservations/views.py
@@ -35,12 +35,13 @@ def make_reservation(request):
         unit = data.get('unit')
         username = data.get('username')
         email = data.get('email')
-        isAvailable, garageNum = is_available(start_date, end_date)
-        isFraud,idFraud = is_fraud(start_date,end_date,unit)
+        
         if start_date == end_date :
             return JsonResponse({'success': False, 'message': 'You need to select a start date and an end date that are different!'})
         if start_date == None or end_date == None :
             return JsonResponse({'success': False, 'message': 'Please fill in all the fields!'})
+        isAvailable, garageNum = is_available(start_date, end_date)
+        isFraud,idFraud = is_fraud(start_date,end_date,unit)
         if isFraud or isAvailable:
             message = "Reservation made successfully, an email with the details has been sent!" if not isFraud else "Your reservation is still on hold, we will contact you for more informations ."
             # Create reservation
