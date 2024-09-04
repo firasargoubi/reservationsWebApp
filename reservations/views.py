30a111dc294b915acc00c66c31596d17200587c4
Firas Argoubi <99605613+firasargoubi@users.noreply.github.com>
2024-09-04 19:57:05 -0400

    Update views.py

--- reservations/views.py
+++ reservations/views.py
@@ -39,11 +39,9 @@ def make_reservation(request):
         isFraud,idFraud = is_fraud(start_date,end_date,unit)
         if start_date == end_date :
             return JsonResponse({'success': False, 'message': 'You need to select a start date and an end date that are different!'})
-        elif not start_date or not end_date or not unit or not username or not email :
+        elif start_date == None or end_date == None :
             return JsonResponse({'success': False, 'message': 'Please fill in all the fields!'})
         if isFraud or isAvailable:
-
-
             message = "Reservation made successfully, an email with the details has been sent!" if not isFraud else "Your reservation is still on hold, we will contact you for more informations ."
             # Create reservation
             client = Client(username = username,email=email,
